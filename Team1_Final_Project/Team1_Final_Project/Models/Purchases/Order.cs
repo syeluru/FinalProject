268368a375dfd500a7e65c593af3feeb8d986932
saiyeluru <syeluru96@gmail.com>
2016-11-12 14:21:11 -0600

    added the order and shopping cart models

---
+++ Team1_Final_Project/Team1_Final_Project/Models/Purchases/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team1_Final_Project.Models.Purchases
+{
+    public class Order
+    {
+        [Required(ErrorMessage = "Order ID is required")]
+        [Display(Name = "Order ID")]
+        public Int16 OrderID { get; set; }
+    }
+}
---
+++ Team1_Final_Project/Team1_Final_Project/Models/Purchases/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Team1_Final_Project.Models.Identity;
+using Team1_Final_Project.Models.Music;
+
+namespace Team1_Final_Project.Models.Purchases
+{
+    public class ShoppingCart
+    {
+        // scalar properties
+        [Required(ErrorMessage = "Shopping Cart ID is required")]
+        [Display(Name = "Shopping Cart ID")]
+        public Int16 ShoppingCartID { get; set; }
+
+        [Required(ErrorMessage = "Total Price is required")]
+        [Display(Name = "Total Price")]
+        public Decimal TotalPrice { get; set; }
+
+        // navigational properties
+        public virtual AppUser Customer { get; set; }
+
+
+    }
+}
